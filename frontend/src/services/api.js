8f671f30f67ed1fcc955d264e15493c039616096
webweaver <upans444@gmail.com>
2026-06-06 14:30:09 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,47 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// const API_URL = 'http://localhost:5000/api';
-const API = axios.create({
-  baseURL: "https://careeros-3-2ujf.onrender.com",
-});
+// // const API_URL = 'http://localhost:5000/api';
+// const API = axios.create({
+//   baseURL: "https://careeros-3-2ujf.onrender.com",
+// });
 
 
+
+// const api = axios.create({
+//   baseURL: API_URL,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// api.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       localStorage.removeItem('token');
+//       window.location.href = '/login';
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
+
+mport axios from 'axios';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: 'https://careeros-3-2ujf.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
